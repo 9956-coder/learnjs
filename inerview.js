2ea63b85ba7ba794fbbd657929ff6babf56da1c4
Rakesh-redapple <rakesh@redappletech.com>
2023-02-15 21:57:02 +0530

    one changes done

---
+++ inerview.js
@@ -0,0 +1,21 @@
+// // wirte a program to remove duplicacy in an array
+// let arr=[1,25,1,2,5,9,5];
+// arr.sort();
+// // removeduplicate([1,2,5,2,1,8]);
+// // console.log(arr.filter((el,index)=>arr.indexOf(el)===index))
+
+// // arr.forEach(el=>console.log(el));
+
+// console.log(arr.includes(50));
+
+// let a=[1,1,5,0,9,1,1,200,5];
+// a.sort();
+// console.log(a.filter((el,index)=>a.indexOf(el)===index));
+
+let a=[1,1,5,0,9,1,1,200,5];
+// let b=a.filter(el=>el+2);
+// console.log(b);
+let c=a.map((el)=>el*2);
+console.log(c);
+
+//sfsfddffbfbnfbn
