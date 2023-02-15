cdd72589a71bf5bf788f198fe894fe91d75cadfc
Rakesh-redapple <rakesh@redappletech.com>
2023-02-15 23:31:56 +0530

    reduce implemented

--- inerview.js
+++ inerview.js
@@ -12,11 +12,14 @@
 // a.sort();
 // console.log(a.filter((el,index)=>a.indexOf(el)===index));
 
-let a=[1,1,5,0,9,1,1,200,5];
-// let b=a.filter(el=>el+2);
-// console.log(b);
-let c=a.map((el)=>el*2);
-console.log(c);
+// let a=[1,1,5,0,9,1,1,200,5];
+// // let b=a.filter(el=>el+2);
+// // console.log(b);
+// let c=a.map((el)=>el*2);
+// console.log(c);
 
 //sfsfddffbfbnfbn
-//sadsadsfsdd
+//sadsadsfsdds
+let a=[10,205,2,0];
+console.log(a.reduce((min,max)=>min<max?min:max));
+console.log(a.reduce((min,max)=>max>min?max:min));
